2987c20c95a4ab581b1f41d7f7e29ddcb8906cce
matthias@arch <matthiasqui@protonmail.com>
2021-03-11 03:25:25 +0100

    Added range_plot

--- tools/plot.py
+++ tools/plot.py
@@ -1,56 +1,33 @@
 import matplotlib.pyplot as plt
-import functions as fun
 import numpy as np
 import re
 
-def rangeplot(f: fun.MathFunction, xmin=0, xmax=1, steps=1000, fig=None, dpi=300, ax=None, title=None, xlabel=None, ylabel=None, xscale="linear", yscale="linear", fontsize="14"):
-    """
-    :param f:       MathFunction object
-    :param xmin:    minimal x-value
-    :param xmax:    maximum x-value
-    :param fig:     pyplot figure object
-    :param steps:   how many points are calculated
-    :param title:   title pf the plot
-    :param xlabel:  x-axis label
-    :param ylabel:  y-axis label
-    :param xscale:  "linear", "log", "symlog", "logit", ..
-    :param yscale:  "linear", "log", "symlog", "logit", ..
-    :return:        matplotlib figure object, matplotlib axes object
-    """
-    # figure:
-    # figsize=None, dpi=None, facecolor=None, edgecolor=None, linewidth=0.0, frameon=None, subplotpars=None, tight_layout=None, constrained_layout=None
-    # create a new figure is none is given:
-    plt.rcParams.update({
-        "font.size": fontsize,
-        "text.usetex": True,
-        "font.family": "sans-serif",
-        "font.sans-serif": ["Helvetica"]})
 
-    if not fig:
-        fig = plt.figure(figsize=(8, 4), dpi=dpi, linewidth=1.0, frameon=None, subplotpars=None, tight_layout=True, constrained_layout=None)
-    if not ax:
-        ax = fig.add_subplot(xlabel=xlabel, ylabel=ylabel, xscale=xscale, yscale=yscale)
-
+def range_plot(f, xmin=0, xmax=1, steps=1000,  **keywords):
+    """
+    Plots a function in an area
+    :param f:           a python function, depending on one variable
+    :param xmin:        lower x-limit
+    :param xmax:        upper x-limit
+    :param steps:       count of points calculated
+    :param keywords:    see 'tools.tool.plot' for all keyword options
+    :return:            matplotlib figure, matplotlib axis
+    """
     dx = abs(xmax - xmin) / steps
     # np.arange seems to leave out the upper value, so x1+dx gives x1 as the highest value
-    x = np.arange(xmin, xmax + dx, dx)
-    y = np.empty([steps + 1])
+    xdata = np.arange(xmin, xmax, dx)
+    ydata = np.empty([steps + 1])
 
     for i in range(0, steps + 1, 1):
-        y[i] = f.calc(x[i])
-    ax.plot(x, y)
-    if title:
-        ax.set_title(title, fontsize=fontsize)
-    # plt.imsave("meinbild.png")
-    # plt.show()
-    print(x, y)
+        ydata[i] = f(xdata[i])
+    fig, ax = plot(xdata, ydata, **keywords)
     return fig, ax
 
 
 def plot(xdata, ydata, marker=None, line="-", color=None, label=None,       # line options
          fig=None, ax=None, dpi=300, figsize=None,                                     # figure/axes options
          title=None, xlabel=None, ylabel=None, legend=False, fontsize="13", # labels....
-         grid="major", gline="-", gcolor="#777",
+         grid="major", gline="-", gcolor="#888",
          xlim=None, ylim=None, xscale="linear", yscale="linear",                                  # axes options
          show=True):
     """
@@ -158,11 +135,12 @@ def plot(xdata, ydata, marker=None, line="-", color=None, label=None,       # li
 
     fsize = None
     # check if valid figsize is given
-    if hasattr(figsize, "len"):
-        print("yayyayayayay")
+
+    try:
         if not isinstance(figsize, str) and len(figsize) == 2:
             fsize = figsize
-
+    except TypeError:
+        pass
 
     # figure:
     # figsize=None, dpi=None, facecolor=None, edgecolor=None, linewidth=0.0, frameon=None, subplotpars=None, tight_layout=None, constrained_layout=None
